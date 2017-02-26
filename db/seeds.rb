29a8b69c0bd8ff9c402f2722257c7217949ca65b
Leanne Eckelberg <leanne.eckelberg@gmail.com>
2017-02-26 13:07:51 -0700

    implemented subscription-based product GROWHACK2

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,24 @@ growth_hacking = Product.create(title: "Growth Hacking", subtitle: "Crash Course
                 <li>How to apply the Lean Marketing Framework</li>
                 <li>Resources and tools you'll need to know</li>
             </ul>})
+growth_hacking_monthly = Product.create(
+	title: "Growth Hacking Monthly",
+	subtitle: "Ongoing Course", 
+	author: "Mattan Griffel",
+	price: "4.99",
+	sku: "GROHACK2",
+	length: "6 months",
+	details: "We'll charge you monthly for unlimited access",
+	author_image_name: "design/teacher-image.jpg", 
+	download_url: "https://s3.amazonaws.com/one_month_stripe_payment/Peter+Bell+Resume.pdf",
+	description: %{
+		 <p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
+     <p>In this series, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+     <p><strong>What You'll Learn</strong></p>
+	    <ul class="no-indent">
+	      <li>What is a growth hacker?</li>
+	      <li>The 5 stages of the user lifecycle</li>
+	      <li>How to apply the Lean Marketing Framework</li>
+	      <li>Resources and tools you'll need to know</li>
+	    </ul>
+		},
